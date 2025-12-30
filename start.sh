58e25db18ad927a8bb6355ed712ee8481382daca
Tyrel Clayton <tyrelclayton92@gmail.com>
2025-12-29 20:03:41 -0500

    pin prisma version in start command

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@
 # run, which is why this file exists in the first place.
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
-npx prisma migrate deploy
+npx prisma@6.3.0 migrate deploy
 npm run start
