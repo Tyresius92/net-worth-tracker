1793b10e1b09429ba2667b4ac09f7a62a730785d
Tyrel Clayton <tyrelclayton92@gmail.com>
2026-01-01 16:46:25 -0500

    add new attributes to user

---
+++ prisma/migrations/20260101214606_add_mfa_attributes_to_user/migration.sql
@@ -0,0 +1,20 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "twoFactorEnabled" BOOLEAN NOT NULL DEFAULT false,
+    "twoFactorSecret" TEXT,
+    "role" TEXT NOT NULL DEFAULT 'customer'
+);
+INSERT INTO "new_User" ("createdAt", "email", "firstName", "id", "lastName", "role", "updatedAt") SELECT "createdAt", "email", "firstName", "id", "lastName", "role", "updatedAt" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
