2f01ca58b22428205c3fcc1fbae925a1263af88d
Tyrel Clayton <tyrelclayton92@gmail.com>
2026-05-03 19:14:32 -0400

    remove profiling package

--- instrument.server.mjs
+++ instrument.server.mjs
@@ -1,6 +1,5 @@
 import process from "node:process";
 import * as Sentry from "@sentry/react-router";
-import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 Sentry.init({
   dsn: "https://6e947b5e11dca72072823b8789a39032@o4511327942475776.ingest.us.sentry.io/4511327945490432",
@@ -10,9 +9,7 @@ Sentry.init({
   // Enable logs to be sent to Sentry
   enableLogs: true,
 
-  integrations: [nodeProfilingIntegration()],
-  tracesSampleRate: 1.0, // Capture 100% of the transactions
-  profilesSampleRate: 1.0, // profile every transaction
+  tracesSampleRate: 1.0,
 
   // Set up performance monitoring
   beforeSend(event) {
