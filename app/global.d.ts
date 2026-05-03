263ec5d97cbc23c1d37d936e39456f8d38569d3d
Tyrel Clayton <tyrelclayton92@gmail.com>
2026-05-03 12:25:08 -0400

    set security headers

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,17 +1,6 @@
-interface Window {
-  ENV: {
-    STRIPE_PUBLIC_KEY: string;
-  };
-}
-
-window.ENV = window.ENV || {};
-
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      STRIPE_SECRET_KEY: string;
-      STRIPE_PUBLIC_KEY: string;
-      STRIPE_WEBHOOK_SECRET: string;
       PRISMA_FIELD_ENCRYPTION_KEY: string;
     }
   }
